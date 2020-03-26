5563dda96f95062ac7d3a8e92442ae07ec1e0380
Bo Maryniuk <bo@maryniuk.net>
2020-03-26 19:20:39 +0100

    Implement event dispatchers for ping

--- wzcd_events.go
+++ wzcd_events.go
@@ -3,16 +3,54 @@ package wzcd
 import (
 	"log"
 
+	"github.com/infra-whizz/wzlib"
+
+	"github.com/davecgh/go-spew/spew"
+	wzlib_transport "github.com/infra-whizz/wzlib/transport"
 	"github.com/nats-io/nats.go"
 )
 
 type WzcDaemonEvents struct {
+	daemon *WzcDaemon
 }
 
-func (wz *WzcDaemonEvents) onConsoleEvent(m *nats.Msg) {
-	log.Println("received from console", len(m.Data), "bytes")
+// NewWzcDaemonEvents creates new instance of the daemon events class
+func NewWzcDaemonEvents(daemon *WzcDaemon) *WzcDaemonEvents {
+	d := new(WzcDaemonEvents)
+	d.daemon = daemon
+	return d
 }
 
-func (wz *WzcDaemonEvents) onResponseEvent(m *nats.Msg) {
-	log.Println("received from response channel", len(m.Data), "bytes")
+// OnConsoleEvent receives and dispatches messages on console channel
+func (wz *WzcDaemonEvents) OnConsoleEvent(m *nats.Msg) {
+	log.Println("On Console channel Event")
+	envelope := wzlib_transport.NewWzEventMsgUtils().GetMessage(m.Data)
+
+	switch envelope.Type {
+	default:
+		log.Println("Discarding unknown message from console channel:")
+		spew.Dump(envelope)
+	}
+}
+
+// OnClientEvent receives and dispatches messages on client channel
+func (wz *WzcDaemonEvents) OnClientEvent(m *nats.Msg) {
+	log.Println("On Client channel Event")
+	envelope := wzlib_transport.NewWzEventMsgUtils().GetMessage(m.Data)
+
+	switch envelope.Type {
+	case wzlib_transport.MSGTYPE_PING:
+		response := wzlib_transport.NewWzMessage(wzlib_transport.MSGTYPE_PING)
+		response.Payload = envelope.Payload
+		msg, _ := response.Serialise()
+		err := wz.daemon.GetTransport().GetPublisher().Publish(wzlib.CHANNEL_CONTROLLER, msg)
+		if err != nil {
+			log.Println("Error sending ping event:", err.Error())
+		}
+	case wzlib_transport.MSGTYPE_REGISTRATION:
+		log.Println("Registration event")
+	default:
+		log.Println("Discarding unknown message from client channel:")
+		spew.Dump(envelope)
+	}
 }
